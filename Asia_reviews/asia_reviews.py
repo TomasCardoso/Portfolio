e8e8fd7c89b4171650557013a94d64d46f90db9a
TomasCardoso <tomascardoso@sapo.pt>
2019-01-17 16:42:41 +0000

    stuff

--- Asia_reviews/asia_reviews.py
+++ Asia_reviews/asia_reviews.py
@@ -7,4 +7,10 @@ def main():
     print(type(dataset))
     print(dataset.iloc[:,2])
 
+    attributes = {'Category 1' : 'Art Galleries',
+                  'Category 2' : 'Dance Clubs',
+                  'Category 3' : 'Juice Bars',
+                  'Category 4' : 'Restaurants',
+                  'Category 5' : 'Museums'}
+
 main()
