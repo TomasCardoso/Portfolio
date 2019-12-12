4225897ad8b9042d5d7aefd0fe95ee11a49fce93
TomasCardoso <tomascardoso@sapo.pt>
2019-12-12 12:00:03 +0000

    Folder changes

---
+++ Energy Consumption prediction/energy_pred.py
@@ -0,0 +1,203 @@
+import pandas as pd
+#import vaex as pd
+#from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from datetime import datetime, date
+from numpy import sin, cos, pi
+import numpy as np
+import xgboost as xgb
+#import time
+import pickle
+#import calendar
+from matplotlib import pyplot
+
+
+# Global variable enconding
+#start = time.time()
+# Variable creation for season computing. 
+Y = 2000
+seasons = [('0', (date(Y,  1,  1),  date(Y,  3, 20))), # Winter
+	           ('1', (date(Y,  3, 21),  date(Y,  6, 20))), # Spring
+	           ('2', (date(Y,  6, 21),  date(Y,  9, 22))), # Summer
+	           ('3', (date(Y,  9, 23),  date(Y, 12, 20))), # Autumn
+	           ('0', (date(Y, 12, 21),  date(Y, 12, 31)))] # Winter
+
+def fetch_data(data_type, encode_categorical_data = True):
+
+	data_path = 'data/' + data_type + '.csv'
+	weather_path = 'data/weather_' + data_type + '.csv'
+
+	building_metadata = pd.read_csv('data/building_metadata.csv')
+	weather = pd.read_csv(weather_path)
+	data = pd.read_csv(data_path)
+
+	data = pd.merge(data, building_metadata, on='building_id')
+	data = pd.merge(data, weather, left_on=['site_id', 'timestamp'], right_on=['site_id', 'timestamp'], how='left')
+
+	if encode_categorical_data:
+		data = encode_timestamp(data)
+		data = encode_categorical(data)
+
+	return data
+
+# Performs variable enconding for categorical fields. 
+# One hot enconding is done by default
+def encode_categorical(data, enconding='one_hot_enconding'):
+
+	if enconding == 'label_encoding':
+		le = preprocessing.LabelEncoder()
+		le.fit(data['primary_use'])
+
+		encoded = le.transform(data['primary_use'])
+
+		data = data.drop('primary_use', axis = 1)
+
+		data['primary_use'] = encoded
+
+	elif enconding == 'one_hot_enconding':
+		encoded_data = pd.get_dummies(data['primary_use'], drop_first = False)
+		data = data.drop('primary_use', axis = 1)
+		data = pd.concat([data, encoded_data], axis=1)
+
+	return data
+
+# Get season of datetime variable
+def get_season(date_var):
+
+	date_var = date(date_var.year, date_var.month, date_var.day)
+	date_var = date_var.replace(year=Y)
+
+	return next(season for season, (start, end) in seasons 
+		if start <= date_var <= end)
+
+# Feature transformation and creation from timestamp.
+# Features created:
+# 			- sinHoD & cosHoD: cyclical encoding of hour of day
+#			- sinDoW & cosDoW: cyclical encoding of day of week
+#			- sinDoM & cosDoM: cyclical encoding of day of month
+#			- sin_season & cos_season: cyclical encoding of season of the year
+def encode_timestamp(data):
+	
+	sinHoD = []
+	cosHoD = []
+	sinDoM = []
+	cosDoM = []
+	sinDoW = []
+	cosDoW = []
+	sin_season = []
+	cos_season = []
+
+	days_in_month = 31
+	hours_in_day = 24
+	days_in_week = 7
+	seasons_in_year = 4
+
+	for i in range(len(data)):
+
+		d = datetime.strptime(data['timestamp'][i], '%Y-%m-%d %H:%M:%S')
+
+		sinHoD.append(sin(2*pi*d.hour/hours_in_day))
+		cosHoD.append(cos(2*pi*d.hour/hours_in_day))
+		sinDoM.append(sin(2*pi*d.day/days_in_month))
+		cosDoM.append(cos(2*pi*d.day/days_in_month))
+		sinDoW.append(sin(2*pi*d.weekday()/days_in_week))
+		cosDoW.append(cos(2*pi*d.weekday()/days_in_week))
+		sin_season.append(sin(2*pi*float(get_season(d))/seasons_in_year))
+		cos_season.append(cos(2*pi*float(get_season(d))/seasons_in_year))
+
+	data['sinHoD'] = sinHoD
+	data['cosHoD'] = cosHoD
+	data['sinDoM'] = sinDoM
+	data['cosDoM'] = cosDoM
+	data['sinDoW'] = sinDoW
+	data['cosDoW'] = cosDoW
+	data['sin_season'] = sin_season
+	data['cos_season'] = cos_season
+
+	data = data.drop('timestamp', axis = 1)
+
+	return data
+
+def model_training():
+
+	train = fetch_data('train')
+
+	model = xgb.XGBRegressor(objective ='reg:squarederror', n_jobs = 5, max_depth = 10, n_estimators = 20, verbosity=2)
+
+	print('Starting model training...')
+	
+	y = train['meter_reading']
+	X = train.drop(['meter_reading'], axis = 1)
+	model.fit(X, y)
+
+	print('Done')
+
+	model.save_model('model/xgboost_complete.model')
+
+def complete_schema(base, current):
+
+	base_columns = list(base.columns.values)
+	current_columns = list(current.columns.values)
+	for column_name in base_columns:
+		if column_name not in current_columns:
+			current[column_name] = np.zeros((len(current)))
+
+	current = current.reindex(columns=base_columns)
+
+	return current
+
+def compute_predictions(train):
+	
+	test = fetch_data('test')
+	test = test.sort_values('row_id')
+	print('Starting prediction...')
+
+	row_id = test['row_id']
+	X = test.drop(['row_id'], axis = 1)
+
+	train = train.drop(['meter_reading', 'Unnamed: 0', 'Unnamed: 0.1', 'Unnamed: 0.1.1'], axis = 1)
+	X = complete_schema(train, X)
+
+	model = xgb.XGBRegressor(objective ='reg:squarederror', n_jobs = 5, max_depth = 10, n_estimators = 20, verbosity=2)
+	model.load_model('model/xgboost_complete.model')
+	y_pred = model.predict(X)
+
+	test['meter_reading'] = y_pred
+	predictions = test[['row_id', 'meter_reading']]
+
+	print('Done')
+
+	return predictions
+
+#Calculate feature importance using XGBoost
+def feature_importance(model):
+
+	xgb.plot_importance(model)
+	pyplot.show()
+
+def rmsle(real, predicted):
+	sum = 0.0
+	real = real.to_list()
+
+	for x in range(len(predicted)):
+		if predicted[x] < 0 or real[x] < 0:
+			continue
+		p = np.log(predicted[x]+1)
+		r = np.log(real[x]+1)
+		sum = sum + (p - r)**2
+
+	return (sum/len(predicted))**0.5
+
+def main():
+
+	#print('Loading data...')
+	#train = load_data()
+	#train.to_csv('complete_training_set.csv')
+	train = pd.read_csv('complete_training_set.csv', nrows=10)
+	# Calculate feature importance
+	#feature_importance(model)
+	model_training()
+	predictions = compute_predictions(train)
+	predictions.to_csv('submission.csv', index=False)
+
+main()
